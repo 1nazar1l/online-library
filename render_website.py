881b9febb3321aa2927d5de9d9d537f084de41e7
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-11-14 17:07:15 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -3,6 +3,7 @@ import json
 from livereload import Server
 import os
 from more_itertools import chunked
+import math
 
 
 def on_reload():
@@ -16,11 +17,13 @@ def on_reload():
 
     os.makedirs("pages", exist_ok=True)
 
+    pages = math.ceil(len(books)/10)
+
     books = list(chunked(books, 10))
     for i, books_on_page in enumerate(books, 1):
 
         template = env.get_template('template.html')
-        rendered_page = template.render(books=books_on_page)
+        rendered_page = template.render(books=books_on_page, pages=pages, page_number=i)
 
         with open(f'pages/index{i}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
