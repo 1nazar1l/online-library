70c61739c8e354f73d3850c6fdb30f2dd02b7e81
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2024-11-30 16:33:45 +0300

    defined the basic type of quotes

--- render_website.py
+++ render_website.py
@@ -12,17 +12,17 @@ import os
 
 def on_reload():
     parser = argparse.ArgumentParser(description="Парсит скачанные книги")
-    parser.add_argument('--dest_folder', type=str, default="media", help="Укажите папку в которую будет сохраняться вся информация")
+    parser.add_argument("--dest_folder", type=str, default="media", help="Укажите папку в которую будет сохраняться вся информация")
     args = parser.parse_args()
     root_folder = args.dest_folder
 
     env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html']),
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html"]),
     )
 
     filepath = os.path.join(sanitize_filename(root_folder),"books_dict.json")
-    with open(filepath, "r", encoding='utf-8') as my_file:
+    with open(filepath, "r", encoding="utf-8") as my_file:
         books = json.load(my_file)
 
     os.makedirs("pages", exist_ok=True)
@@ -34,14 +34,14 @@ def on_reload():
 
     for i, books_on_page in enumerate(books, 1):
         books = list(chunked(books_on_page, books_in_row))
-        template = env.get_template('template.html')
+        template = env.get_template("template.html")
         rendered_page = template.render(books=books, pages=pages, page_number=i)
 
-        filepath = f'pages/index{i}.html'
+        filepath = f"pages/index{i}.html"
         if i == 1: 
-            filepath = f'pages/index.html'
+            filepath = f"pages/index.html"
 
-        with open(filepath, 'w', encoding="utf8") as file:
+        with open(filepath, "w", encoding="utf8") as file:
             file.write(rendered_page)
 
 
@@ -49,7 +49,7 @@ def main():
     on_reload()
     server = Server()
     server.watch("template.html", on_reload)
-    server.serve(root='.', default_filename="pages/index.html")
+    server.serve(root=".", default_filename="pages/index.html")
 
 
 if __name__ == "__main__":
